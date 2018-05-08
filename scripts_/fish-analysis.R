0c350e3e3b83fdb8e2d2d6e054849a65120f07ab
Maya Garber-Yonts <mayagy@uw.edu>
2018-05-08 13:47:42 -0700

    new df with fish with scale length of >1mm

--- scripts_/fish-analysis.R
+++ scripts_/fish-analysis.R
@@ -2,8 +2,13 @@ fish_data <- read.csv("data_/Gaeta_etal_CLC_data.csv")
 
 library(dplyr)
 
-fish_data_cat = fish_data %>%
-  mutate(length_cat = ifelse(length > 200, "big", "small"))
+#this pipes in the inital data and creates a column with fish length
 
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+# new data frame with only fish with a scale length of > 1mm r
+
+fish_data_cat = fish_data %>%
+  mutate(length_cat = ifelse(length > 300, "big", "small")) %>% 
+  filter(scalelength > 1)
