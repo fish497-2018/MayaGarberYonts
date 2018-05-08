a59088fec26eec9d9f5d50883cefcb9da79170e7
Maya Garber-Yonts <mayagy@uw.edu>
2018-05-08 14:29:45 -0700

    histogram of lenth/scale length

--- scripts_/fish-analysis.R
+++ scripts_/fish-analysis.R
@@ -1,6 +1,7 @@
 fish_data <- read.csv("data_/Gaeta_etal_CLC_data.csv")
 
 library(dplyr)
+library(ggplot2)
 
 #this pipes in the inital data and creates a column with fish length
 
@@ -12,3 +13,7 @@ fish_data_cat = fish_data %>%
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small")) %>% 
   filter(scalelength > 1)
+
+ggplot(fish_data_cat, aes(length, scalelength, color=lakeid))+
+  geom_point()
+                        
