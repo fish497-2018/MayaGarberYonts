dea9c72672fbfd359a1df794f99353df32532f51
Maya Garber-Yonts <mayagy@uw.edu>
2018-05-11 14:19:36 -0700

    # 3 filtered out scale length less than 1mm

--- scripts_/fish-analysis.R
+++ scripts_/fish-analysis.R
@@ -3,24 +3,28 @@
 library(dplyr)
 library(ggplot2)
 
+#reads in our data
+fish_data <- read.csv("Gaeta_etal_CLC_data_1.csv")
+
 # 1 
 #GitHub accout created
 
 # 2 Solo Commit
-#reads in our data
-fish_data <- read.csv("Gaeta_etal_CLC_data_1.csv")
-
-
 #this pipes in the inital data and creates a column with fish length
-
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
+# 3
 # new data frame with only fish with a scale length of > 1mm r
-
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small")) %>% 
   filter(scalelength > 1)
+
+# 4 Commit Multiple Files
+
+
+
+
 
 #Histogram, lake id by color
 ggplot(fish_data_cat, aes(length, scalelength, color=lakeid))+
